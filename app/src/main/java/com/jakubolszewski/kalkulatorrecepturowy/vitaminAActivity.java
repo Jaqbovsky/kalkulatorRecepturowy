426c2b71f7b411014e260b7c6e1725c828734fb9
Jaqbovsky <47501201+Jaqbovsky@users.noreply.github.com>
2020-05-15 15:03:45 +0200

    version 0.04 -from Vit A Hasco to Vit A Medana

--- app/src/main/java/com/jakubolszewski/kalkulatorrecepturowy/vitaminAActivity.java
+++ app/src/main/java/com/jakubolszewski/kalkulatorrecepturowy/vitaminAActivity.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +30,11 @@ public class vitaminAActivity extends AppCompatActivity implements AdapterView.O
     Button calc_btn;
     Spinner spinnerCompany, spinnerUnit;
     TextView calculation1_TV, calculation2_TV, calculation3_TV, calculation4_TV;
+    TextView calculation5_TV, calculation6_TV, calculation7_TV, calculation8_TV;
+    TextView calculation9_TV, calculation10_TV, calculation11_TV, calculation12_TV;
+    TextView textView_text1, textView_text2 , textView_text3, textView_text4, textView_text5, textView_text6;
+    TextView textView_vitMain;
+    LinearLayout linearLayout1, linearLayout2;
 
     @SuppressLint("RestrictedApi")
     @Override
@@ -51,6 +57,25 @@ public class vitaminAActivity extends AppCompatActivity implements AdapterView.O
         calculation2_TV = findViewById(R.id.textView_calculation2);
         calculation3_TV = findViewById(R.id.textView_calculation3);
         calculation4_TV = findViewById(R.id.textView_calculation4);
+        calculation5_TV = findViewById(R.id.textView_calculation5);
+        calculation6_TV = findViewById(R.id.textView_calculation6);
+        calculation7_TV = findViewById(R.id.textView_calculation7);
+        calculation8_TV = findViewById(R.id.textView_calculation8);
+        calculation9_TV = findViewById(R.id.textView_calculation9);
+        calculation10_TV = findViewById(R.id.textView_calculation10);
+        calculation11_TV = findViewById(R.id.textView_calculation11);
+        calculation12_TV = findViewById(R.id.textView_calculation12);
+
+        textView_text1 = findViewById(R.id.textView_text1);
+        textView_text2 = findViewById(R.id.textView_text2);
+        textView_text3 = findViewById(R.id.textView_text3);
+        textView_text4 = findViewById(R.id.textView_text4);
+        textView_text5 = findViewById(R.id.textView_text5);
+        textView_text6 = findViewById(R.id.textView_text6);
+
+        textView_vitMain = findViewById(R.id.textView_mainVit);
+
+
 
         //-----------------------------Spinner---------------------------\\
         spinnerCompany = findViewById(R.id.spinner_Company);
@@ -75,12 +100,21 @@ public class vitaminAActivity extends AppCompatActivity implements AdapterView.O
         //-----------------------------TextEdit---------------------------\\
         amount_ET = findViewById(R.id.editText_amount);
 
+        //-----------------------------Linear-Layout---------------------------\\
+        linearLayout1 = findViewById(R.id.linearLayout1);
+        linearLayout2 = findViewById(R.id.linearLayout2);
+
+        linearLayout1.setVisibility(View.GONE);
+        linearLayout2.setVisibility(View.GONE);
         //-----------------------------Button---------------------------\\
         calc_btn = findViewById(R.id.button_calc);
         calc_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String valueFromET = amount_ET.getText().toString();
+
+                linearLayout1.setVisibility(View.VISIBLE);
+                linearLayout2.setVisibility(View.VISIBLE);
 
                 if (!valueFromET.isEmpty()) {
                     amount = Integer.parseInt(valueFromET);
@@ -90,6 +124,9 @@ public class vitaminAActivity extends AppCompatActivity implements AdapterView.O
                 //-----------------------------HASCO---------------------------\\
                 if (companyChoice == 1) {
 
+                    textView_vitMain.setText("Vit A Hasco");
+                    textView_text3.setText("Vit A Medana");
+                    textView_text5.setText("Vit A Fargon");
 
                     String test = String.valueOf(volumeHasco);
 
@@ -99,23 +136,22 @@ public class vitaminAActivity extends AppCompatActivity implements AdapterView.O
                         gramsHasco = amount;
 
                         volumeHasco = gramsHasco / 1.148;
-                        dropsHasco = volumeHasco * 28;
-                        massUnitHasco = volumeHasco * 45000;
+
 
 
                         volumeHasco *= 100;
                         volumeHasco = Math.round(volumeHasco);
                         volumeHasco /= 100;
 
+                        dropsHasco = volumeHasco * 28;
+                        massUnitHasco = volumeHasco * 45000;
+
                         dropsHasco = Math.round(dropsHasco);
                         String drops_String = String.valueOf(dropsHasco);
                         drops_String = drops_String.replace(".0", "");
 
-
-
                         massUnitHasco = Math.round(massUnitHasco);
                         String massUnit_String = String.valueOf(massUnitHasco).replace(".0", "");
-
 
                         calculation1_TV.setText(volumeHasco + "ml");
                         calculation2_TV.setText(drops_String + " kropli");
@@ -129,12 +165,13 @@ public class vitaminAActivity extends AppCompatActivity implements AdapterView.O
                         massUnitHasco = amount;
 
                         volumeHasco = massUnitHasco / 45000;
-                        dropsHasco = volumeHasco * 28;
-                        gramsHasco = volumeHasco * 1.148;
 
                         volumeHasco *= 100;
                         volumeHasco = Math.round(volumeHasco);
                         volumeHasco /= 100;
+
+                        dropsHasco = volumeHasco * 28;
+                        gramsHasco = volumeHasco * 1.148;
 
                         dropsHasco = Math.round(dropsHasco);
                         String drops_String = String.valueOf(dropsHasco).replace(".0", "");
@@ -153,10 +190,32 @@ public class vitaminAActivity extends AppCompatActivity implements AdapterView.O
 
                     }
                     //-----------------------------A-Hasco-na-A-Medana---------------------------\\
+                    massUnitMedana = massUnitHasco;
+
                     volumeMedana = massUnitHasco/50000;
+
+                    volumeMedana *= 100;
+                    volumeMedana = Math.round(volumeMedana);
+                    volumeMedana /= 100;
+
                     dropsMedana = volumeMedana * 30;
                     gramsMedana = volumeMedana * 1.08;
 
+                    gramsMedana *= 100;
+                    gramsMedana = Math.round(gramsMedana);
+                    gramsMedana /= 100;
+
+                    dropsMedana = Math.round(dropsMedana);
+                    String drops_String = String.valueOf(dropsMedana).replace(".0", "");
+
+                    massUnitHasco = Math.round(massUnitMedana);
+                    String massUnit_String = String.valueOf(massUnitMedana).replace(".0", "");
+
+                    calculation5_TV.setText(gramsMedana + " g");
+                    calculation6_TV.setText(volumeMedana + " ml");
+                    calculation7_TV.setText(drops_String + " kropli");
+                    calculation8_TV.setText(massUnit_String + " j.m.");
+                    textView_text3.setText("Vit A Medana");
 
                 }
                 //-----------------------------MEDANA---------------------------\\
